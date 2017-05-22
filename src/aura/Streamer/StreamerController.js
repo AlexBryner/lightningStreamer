404d94377de1ebf2b8040575db1db9a1fdfb7728
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-05-22 14:39:35 -0500

    streamer for platform events

--- src/aura/Streamer/StreamerController.js
+++ src/aura/Streamer/StreamerController.js
@@ -1,33 +1,35 @@
+/* global $ */
+
 ({
     doInit : function(component, event, helper) {
         console.log('doing init on streamer');
-        
+
         // Retrieve the session id and initialize cometd
-        var sessionAction = component.get("c.sessionId");
-        
+        let sessionAction = component.get("c.sessionId");
+        let url = helper.buildUrl(component);
+
         sessionAction.setCallback(this, function(response) {
-            var state = response.getState();
+            let state = response.getState();
             if(state  === "SUCCESS") {
-                var sessionId = response.getReturnValue();
-                var authstring = "OAuth " + sessionId;
-                
+                let sessionId = response.getReturnValue();
+                let authstring = "OAuth " + sessionId;
+
                 //authenticate to the Streaming API
                 $.cometd.init({
-                    url: window.location.protocol + '//' + window.location.hostname + '/cometd/36.0/',
+                    url: window.location.protocol + '//' + window.location.hostname + '/cometd/40.0/',
                     requestHeaders: { Authorization: authstring },
                     appendMessageTypeToURL : false
                 });
-                $.cometd.subscribe('/topic/'+component.get("v.topic"), function (message){
-                    //placeholder.  for real, emit as a message event
-                    console.log(message);
-                    console.log(message.data.sobject);
-                    var appEvent = $A.get("e.c:StreamerEvent");
-					appEvent.setParams({ "message" : message });
-					appEvent.fire();
+
+                $.cometd.subscribe(url, function (message){
+                    let evt = $A.get("e.ltng:sendMessage");
+                    evt.setParams({"message" : message, "channel" : "streamingAPISubscriber"});
+                    evt.fire();
                 });
-                	
             }
+
         });
-        $A.enqueueAction(sessionAction);        
+
+        $A.enqueueAction(sessionAction);
     }
 })
